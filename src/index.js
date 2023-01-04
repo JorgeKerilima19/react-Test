014cbdc03ae1a92312863ca7ce25776c59f25e7e
JorgeMioska <quispechilojorge@gmail.com>
2023-01-04 04:38:06 -0500

    my first component

--- src/index.js
+++ src/index.js
@@ -1,5 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const root=ReactDOM.createRoot(document.getElementById("root"));
-root.render(<h1>Hello World!</h1>);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+function Greeting() {
+  return (
+    <section>
+      <h1>Hello World</h1>
+      <p>Hola Xd</p>
+    </section>
+  );
+}
+
+root.render(
+  <div>
+    <Greeting />
+  </div>
+);
