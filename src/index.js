c7cdd6ddad00287d6009e35ffe00de216bab7ae4
JorgeMioska <quispechilojorge@gmail.com>
2023-01-04 04:43:33 -0500

    trying jsx

--- src/index.js
+++ src/index.js
@@ -4,10 +4,14 @@ import ReactDOM from "react-dom/client";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 function Greeting() {
+  const name="Jorge";
+  const age=21;
+  const married=true
   return (
     <section>
-      <h1>Hello World</h1>
-      <p>Hola Xd</p>
+      <h1>Hello {name}</h1>
+      <p>5 years ago you were {age - 5}</p>
+      <p>you are: {married? "married":"single"}</p>
     </section>
   );
 }
