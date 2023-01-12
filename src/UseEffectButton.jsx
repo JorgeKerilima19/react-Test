139b7cc94606c8a6daf71d66b698c5c1d6b2861c
JorgeMioska <quispechilojorge@gmail.com>
2023-01-12 06:35:34 -0500

    useEffect ways to use

--- src/UseEffectButton.jsx
+++ src/UseEffectButton.jsx
@@ -4,8 +4,10 @@ export function InputEffect() {
   const [number, setNumber] = useState(50);
     useEffect(()=>{
         console.log("Watching state");
-    })
+    },[number])
     //Just One argument to watch the component State in general
+    //Empty array to get executed once
+    //An argument to make a dependency
   return (
     <>
       <h1>{number}</h1>
