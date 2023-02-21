1d7b109f999e3dfb72d864034ea6a12a1d23275c
JorgeMioska <quispechilojorge@gmail.com>
2023-02-21 00:26:52 -0500

    users

--- src/helpersUseEffect/getUser.jsx
+++ src/helpersUseEffect/getUser.jsx
@@ -1,8 +1,33 @@
+const users = [
+  {
+    id: 1,
+    name: "Jose",
+    email: "email@email.com",
+  },
+  {
+    id: 2,
+    name: "Maria",
+    email: "email2@email.com",
+  },
+];
 
 function GetUser() {
   return (
-    <div>GetUser</div>
-  )
+    <div>
+      <ul style={{ color: "red", listStyle: "none" }}>
+        {users.map((user) => {
+          return (
+            <li>
+              <h2>
+                {user.id} : {user.name}
+              </h2>
+              <h4>{user.email}</h4>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
 }
 
-export default GetUser
+export default GetUser;
